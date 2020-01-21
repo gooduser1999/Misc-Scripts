ac0add20ce9647432f139c3ca5ee616dd0a43cad
gooduser1999 <49349223+gooduser1999@users.noreply.github.com>
2020-01-21 06:01:35 +0000

    Update Upload-Chunks64.ps1

--- Upload-Chunks64.ps1
+++ Upload-Chunks64.ps1
@@ -1,3 +1,4 @@
+function UploadChunks {
 param(
 	[String] $data = $args[0],
 	[String] $Uri = $args[1]
@@ -8,7 +9,7 @@ $url = ($Uri + '/' + $fileName);
 $content = Get-Content -LiteralPath ($data) -Encoding UTF8 -ErrorAction SilentlyContinue
 $scriptInp = [string]::Join("`r`n", $content)
 $buffer64 = [System.Convert]::ToBase64String(([System.Text.Encoding]::UTF8.GetBytes($scriptInp))) 
-buffer = ([text.encoding]::UTF8).GetBytes($buffer64);
+$buffer = ([text.encoding]::UTF8).GetBytes($buffer64);
 [System.Net.HttpWebRequest] $webRequest = [System.Net.WebRequest]::Create($url)
 $webRequest.Timeout = $timeout
 $webRequest.Method = "POST"
@@ -23,3 +24,4 @@ $streamReader = New-Object System.IO.StreamReader($webResponse.GetResponseStream
 $result = $streamReader.ReadToEnd()
 return $result
 $stream.Close() 
+}
