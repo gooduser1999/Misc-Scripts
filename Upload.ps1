322a688993559d14864ad399d0c8ca06607a1a12
gooduser1999 <49349223+gooduser1999@users.noreply.github.com>
2020-01-25 12:43:40 -0600

    Update and rename Upload-Chunks64.ps1 to Upload.ps1

---
+++ Upload.ps1
@@ -0,0 +1,50 @@
+function Upload {
+param(
+	[String] $data = $args[0],
+	[String] $Uri = $args[1],
+	[Switch] $String
+	)
+
+if ($String) {
+$Timeout=10000000;
+$buffer64 = [System.Convert]::ToBase64String(([System.Text.Encoding]::UTF8.GetBytes($data))) 
+$buffer = ([text.encoding]::UTF8).GetBytes($buffer64);
+[System.Net.HttpWebRequest] $webRequest = [System.Net.WebRequest]::Create($Uri) 
+$webRequest.Timeout = $timeout
+$webRequest.Method = "POST"
+$webRequest.ContentType = "application/data"
+$webRequest.ContentLength = $buffer.Length;
+$requestStream = $webRequest.GetRequestStream()
+$requestStream.Write($buffer, 0, $buffer.Length)
+$requestStream.Flush()
+$requestStream.Close()
+[System.Net.HttpWebResponse] $webResponse = $webRequest.GetResponse()
+$streamReader = New-Object System.IO.StreamReader($webResponse.GetResponseStream())
+$result = $streamReader.ReadToEnd()
+return $result
+$stream.Close()
+}
+else {
+$Timeout=10000000;
+$fileName = [System.IO.Path]::GetFileName($data)
+$url = ($Uri + '/' + $fileName);
+$content = Get-Content -LiteralPath ($data) -Encoding UTF8 -ErrorAction SilentlyContinue
+$scriptInp = [string]::Join("`r`n", $content)
+$buffer64 = [System.Convert]::ToBase64String(([System.Text.Encoding]::UTF8.GetBytes($scriptInp))) 
+$buffer = ([text.encoding]::UTF8).GetBytes($buffer64);
+[System.Net.HttpWebRequest] $webRequest = [System.Net.WebRequest]::Create($url) 
+$webRequest.Timeout = $timeout
+$webRequest.Method = "POST"
+$webRequest.ContentType = "application/data"
+$webRequest.ContentLength = $buffer.Length;
+$requestStream = $webRequest.GetRequestStream()
+$requestStream.Write($buffer, 0, $buffer.Length)
+$requestStream.Flush()
+$requestStream.Close()
+[System.Net.HttpWebResponse] $webResponse = $webRequest.GetResponse()
+$streamReader = New-Object System.IO.StreamReader($webResponse.GetResponseStream())
+$result = $streamReader.ReadToEnd()
+return $result
+$stream.Close()
+}
+}
