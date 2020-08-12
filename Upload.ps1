1d7046300ae0c7e39047e58ed604427abe69ca7e
gooduser1999 <49349223+gooduser1999@users.noreply.github.com>
2020-08-12 11:23:24 -0500

    Update Upload.ps1

--- Upload.ps1
+++ Upload.ps1
@@ -2,7 +2,8 @@ function Upload {
 param(
 	[String] $data = $args[0],
 	[String] $Uri = $args[1],
-	[Switch] $String
+	[Switch] $String,
+	[Switch] $Byte
 	)
 
 if ($String) {
@@ -10,6 +11,28 @@ $Timeout=10000000;
 $buffer64 = [System.Convert]::ToBase64String(([System.Text.Encoding]::UTF8.GetBytes($data))) 
 $buffer = ([text.encoding]::UTF8).GetBytes($buffer64);
 [System.Net.HttpWebRequest] $webRequest = [System.Net.WebRequest]::Create($Uri) 
+$webRequest.Timeout = $timeout
+$webRequest.Method = "POST"
+$webRequest.ContentType = "application/data"
+$webRequest.ContentLength = $buffer.Length;
+$requestStream = $webRequest.GetRequestStream()
+$requestStream.Write($buffer, 0, $buffer.Length)
+$requestStream.Flush()
+$requestStream.Close()
+[System.Net.HttpWebResponse] $webResponse = $webRequest.GetResponse()
+$streamReader = New-Object System.IO.StreamReader($webResponse.GetResponseStream())
+$result = $streamReader.ReadToEnd()
+return $result
+$stream.Close()
+}
+if ($Byte) {
+$Timeout=10000000;
+$fileName = [System.IO.Path]::GetFileName($data)
+$url = ($Uri + '/' + $fileName);
+$content = Get-Content -LiteralPath ($data) -Encoding byte -ErrorAction SilentlyContinue
+$buffer64 = [convert]::ToBase64String($content)
+$buffer = ([text.encoding]::UTF8).GetBytes($buffer64);
+[System.Net.HttpWebRequest] $webRequest = [System.Net.WebRequest]::Create($url) 
 $webRequest.Timeout = $timeout
 $webRequest.Method = "POST"
 $webRequest.ContentType = "application/data"
